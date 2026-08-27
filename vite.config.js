a41266e3cca67e4da087cc52618c6ae7c2533672
SISVOR 009 Dev <dev@sisvor009.local>
2026-08-27 08:25:20 +0700

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   base: '/website-Kelas/',
+
   build: {
     outDir: 'dist',
   },
+
   server: {
     proxy: {
       '/api': {
@@ -14,11 +17,12 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+
       '/uploads': {
-        target: 'http://localhost:5000', // arahkan ke backend yang sama
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-});
+      },
+    },
+  },
+})
